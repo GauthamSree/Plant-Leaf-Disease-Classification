4399d96c6a597297778607342ffbf7f665250167
Gautham Sreekumar <gauthamsreekumar2001@gmail.com>
2021-04-30 20:55:04 +0530

    small change

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -12,7 +12,7 @@ import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   
   const instance = axios.create({
-     baseURL: "https://plant-disease-backend.herokuapp.com/api"
+     baseURL: "http://0.0.0.0:8000/api"
   })
 
   const [prediction, setPrediction] = useState([])
