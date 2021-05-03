0847cf30250e22f19834fc8ba7bd54eae6dc4cda
Gautham Sreekumar <gauthamsreekumar2001@gmail.com>
2021-05-03 21:50:06 +0530

    minor change

--- Frontend/src/Components/Header.js
+++ Frontend/src/Components/Header.js
@@ -9,7 +9,7 @@ const Header = () =>  {
             <nav>
                 <ul className="nav_links">
                     <li><NavLink to='/prediction_classes' activeClassName="is_active" exact={true}><i className="fas fa-scroll"></i> Prediction Labels</NavLink></li>
-                    <li><a href="https://github.com/GauthamSree" rel="noreferrer" target="_blank"><i className="fab fa-github"></i> GitHub</a></li>
+                    <li><a href="https://github.com/GauthamSree/Plant-Leaf-Disease-Classification" rel="noreferrer" target="_blank"><i className="fab fa-github"></i> GitHub</a></li>
                     <li><a href="http://linkedin.com/in/gautham-sreekumar-5662b318b" rel="noreferrer" target="_blank"><i className="fab fa-linkedin"></i> LinkedIn</a></li>
                 </ul>
             </nav>
